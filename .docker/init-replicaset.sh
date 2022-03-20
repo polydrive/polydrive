89b16d1321e76063ff79b5fdd0bc0b371f832f61
Thomas Gouveia <47056759+thomasgouveia@users.noreply.github.com>
2022-03-20 14:45:25 +0100

    chore: fix dev tools not working on macOS

--- .docker/init-replicaset.sh
+++ .docker/init-replicaset.sh
@@ -2,19 +2,19 @@
 
 # Initiate the replica set into one of mongo instance
 echo "Starting replica set initialization"
-until mongo --host localhost:27017 --eval "print(\"Waited for connection\")"
+until mongo --host mongo-rs-1:27017 --eval "print(\"Waited for connection\")"
 do
     sleep 2
 done
 echo "Connection finished"
 echo "Creating replica set"
-mongo --host localhost:27017 <<EOF
+mongo --host mongo-rs-1:27017 <<EOF
     rs.initiate({
         _id : 'polydrive-rs',
         members: [
-            { _id : 0, host : "localhost:27017" },
-            { _id : 1, host : "localhost:27018" },
-            { _id : 2, host : "localhost:27019", arbiterOnly: true }
+            { _id : 0, host : "mongo-rs-1:27017" },
+            { _id : 1, host : "mongo-rs-2:27017" },
+            { _id : 2, host : "mongo-rs-3:27017", arbiterOnly: true }
         ]
       }
     )
